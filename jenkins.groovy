07c093e53a5600b9ee218254bacb9b636a426785
Zahoor Farooq <89144579+zahoorfarooq@users.noreply.github.com>
2024-12-18 14:42:23 +0530

    Update jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -1,15 +1,15 @@
 pipeline {
     agent { label 'CI_Windows_Slave' }
     environment {
-        SERVICE_REPO = 'https://github.com/BQE/BQECore-Jenkins-CommonLib.git'
-        SCRIPT_REPO = 'https://github.com/BQE/BQECore-Jenkins-CommonLib.git'
-        GET_IP_SCRIPT = "${WORKSPACE}\\Common\\scripts\\fetch_ips.py"
-        PIPELINE = "${WORKSPACE}\\Common\\pipelines\\BQECORE_FETCH_IPS.groovy"
+        SERVICE_REPO = 'https://github.com/X.git'
+        SCRIPT_REPO = 'https://github.com/X.git'
+        GET_IP_SCRIPT = "${WORKSPACE}\\Common\\scripts\\fetch_ip.py"
+        PIPELINE = "${WORKSPACE}\\Common\\pipelines\\jenkins.groovy"
     }
 
     parameters {
-        choice(name: 'EnvironmentGroup', choices: ['fdnlan', 'ecolab', 'batlab', 'DT'], description: 'Select the environment group')
-        choice(name: 'InstanceAttribute', choices: ['fetch_ips'], description: 'Select the instance attribute')
+        choice(name: 'EnvironmentGroup', choices: ['ECOLAB','YODALAB', 'ANALYTICSLAB', 'FDNLAB','BATLAB', 'CAPLAB', 'DT'], description: 'Select the environment group')
+        choice(name: 'InstanceAttribute', choices: ['fetch_ip'], description: 'Select the instance attribute')
     }
 
     stages {
@@ -17,11 +17,14 @@ pipeline {
             steps {
                 script {
                     dir("${WORKSPACE}\\Common") {
-                        checkout changelog: true, poll: false, scm: (
-                            [$class: 'GitSCM', branches: [[name: '*/bqe-devops']], doGenerateSubmoduleConfigurations: false,
-                            extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[$class: 'SparseCheckoutPath', path: '/scripts/']]]],
-                            submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'git_credentials', url: "$SCRIPT_REPO"]]]
-                        )
+                        checkout changelog: true, poll: false, scm: [
+                            $class: 'GitSCM', 
+                            branches: [[name: '*/bqe-devops']],
+                            doGenerateSubmoduleConfigurations: false, 
+                            extensions: [[$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[$class: 'SparseCheckoutPath', path: 'scripts/']]]],
+                            submoduleCfg: [],
+                            userRemoteConfigs: [[credentialsId: 'git_credentials', url: "$SCRIPT_REPO"]]
+                        ]
                     }
                 }
             }
@@ -31,25 +34,31 @@ pipeline {
                 script {
                     echo "Fetching EC2 IP addresses for Environment Group: ${params.EnvironmentGroup} with Attribute: ${params.InstanceAttribute}"
 
-                    if (params.InstanceAttribute == 'fetch_ips') {
-                        // Set the environment variables to pass to the Python script
-                        withEnv([
-                            "EnvironmentGroup=${params.EnvironmentGroup}",
-                            "INSTANCE_ATTRIBUTE=${params.InstanceAttribute}"
-                        ]) {
-                            def command = "python ${GET_IP_SCRIPT}"
-                            try {
-                                // Execute the Python script and capture output
-                                def ipAddresses = bat(script: command, returnStdout: true).trim()
-                                if (ipAddresses) {
-                                    echo "Retrieved IP addresses:\n${ipAddresses}"
-                                } else {
-                                    echo "No running instances found for Environment Group: ${params.EnvironmentGroup}"
-                                }
-                            } catch (Exception e) {
-                                echo "General Exception caught: ${e.message}"
+                    withEnv([
+                        "EnvironmentGroup=${params.EnvironmentGroup}",
+                        "INSTANCE_ATTRIBUTE=${params.InstanceAttribute}"
+                    ]) {
+
+                        try{
+                            println "Calling python script"
+							bat " python ${GET_IP_SCRIPT} -e ${EnvironmentGroup} -i ${INSTANCE_ATTRIBUTE}"
+						}
+						catch (Exception e){
+							println "Couldn't fetch Ips: ${e.message}"
+						}
+                        /*def command = "python ${GET_IP_SCRIPT} -e ${params.EnvironmentGroup} -i ${params.InstanceAttribute}"
+                        try {
+                            def ipAddresses = bat(script: command, returnStdout: true).trim()
+                            if (ipAddresses) {
+                                echo "Retrieved IP addresses:\n${ipAddresses}"
+                            } else {
+                                echo "No running instances found for Environment Group: ${params.EnvironmentGroup}"
                             }
+                        } catch (Exception e) {
+                            echo "General Exception caught: ${e.message}"
                         }
+                        */
+                    
                     }
                 }
             }
